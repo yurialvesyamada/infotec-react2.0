ac3bb9900e535dc30f90a1ac48bb6408aec7a495
Yuri <yurialvesyamada@hotmail.com>
2020-11-10 23:53:18 -0300

    Correção e Alinhamento dos Produtos

--- contato.php
+++ contato.php
@@ -3,11 +3,11 @@
 
 <div class="container-fluid text-center text-info mb-4">
     <h2 class="display-4 my-4">Redes Sociais</h2>
-    <div class="row">
+    <div class="row py-5">
         <div class="col">
             <h5 class="logo"><a href="" title="© 2020 Copyright InfoTec">
                     <img src="assets/img/email-logo2.png" alt="Email InfoTec" width="100px"></a>
-                <p class="my-2">Lojas@infotec.com</p>
+                <p class="my-2">lojas@infotec.com</p>
             </h5>
         </div>
         <div class="col">
--- produtos.php
+++ produtos.php
@@ -1,25 +1,128 @@
 <!-- Conexões com PHP/includes  -->
 <?php require("includes\cabecalho.php") ?>
 
-<div class="container-fluid text-center">
-  <h2 class="display-4 bg-light text-info">Produtos</h2>
+<div class="display-4 text-center text-info my-3">Categorias</div>
+
+<!-- Produtos/Categorias -->
+<div class="container-fluid mb5">
   <div class="row">
-    <?php for ($i = 0; $i < 10; $i++) { ?>
-      <div class="card shadow bg-white rounded col-12 col-md-6 col-lg-3">
-        <img src="assets\img\notebook_acer_gamer.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Notebook Acer</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+    <div class="col-md-2 bg-info text-white rounded">
+      <div class="p-1">
+        <h6 class="text-uppercase">Produtos</h6>
+        <div class="p-lists">
+          <div class="d-flex justify-content-between mt-2"> <span>Todos</span> <span>16</span> </div>
+          <div class="d-flex justify-content-between mt-2"> <span>Notebooks</span> <span>8</span> </div>
+          <div class="d-flex justify-content-between mt-2"> <span>Placa de Vídeo</span> <span>4</span> </div>
+          <div class="d-flex justify-content-between mt-2"> <span>Processadores</span> <span>2</span> </div>
+          <div class="d-flex justify-content-between mt-2"> <span>Memória RAM</span> <span>1</span> </div>
+          <div class="d-flex justify-content-between mt-2"> <span>Monitores</span> <span>1</span> </div>
+          <div class="d-flex justify-content-between mt-2"> <span>CPU</span> <span>Em breve</span> </div>
         </div>
       </div>
-    <?php } ?>
+      <!-- Processadores Checkbox -->
+      <div class="content text-center">
+      <div class="p-2 border-top border-light">
+        <div class="heading d-flex justify-content-between align-items-center">
+          <h6 class="text-uppercase">Processadores</h6> <span>--</span>
+        </div>
 
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Intel Core i9 </label> </div> <span>1</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Intel Core i7 </label> </div> <span>4</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Intel Core i5 </label> </div> <span>2</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> AMD Ryzen 7 </label> </div> <span>2</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> AMD Ryzen 5 </label> </div> <span>2</span>
+        </div>
+
+      </div>
+      <!--  Marcas Checkbox -->
+      <div class="p-2 border-top border-light">
+        <div class="heading d-flex justify-content-between align-items-center">
+          <h6 class="text-uppercase">Marcas</h6> <span>--</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Samsung </label> </div> <span>1</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Acer </label> </div> <span>4</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Sony </label> </div> <span>2</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Dell </label> </div> <span>2</span>
+        </div>
+
+        <div class="d-flex justify-content-between mt-2">
+          <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked> <label class="form-check-label" for="flexCheckChecked"> Lenovo </label> </div> <span>2</span>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  
+    <div class="card-deck mx-auto w-75 h-50 text-center align-items-top">
+      <div class="card">
+        <img src="assets\img\placa-de-video-gtx.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="assets\img\placa-de-video-gtx.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="assets\img\placa-de-video-gtx.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="assets\img\placa-de-video-gtx.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+
+    </div>
   </div>
 </div>
 
 
+
 <!-- Rodapé Includes -->
 <?php require("includes/rodape.php") ?>
+
+
+
