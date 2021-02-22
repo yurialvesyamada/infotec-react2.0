6f0c849b4c1bbc85ff306ae9d99baacea7791eee
Yuri <you@teste.com>
2021-02-22 17:32:49 -0300

    Update Page Contato

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,69 @@
 import React from "react";
-import { Text, ScrollView, StyleSheet, View } from "react-native";
+import { Text, ScrollView, StyleSheet, View, Image } from "react-native";
 
 const Contact = (props) => {
     return (
         <ScrollView>
-            <View style={product.container}>
-                <Text style={product.text}>Contato</Text>
-            </View>
+            <View style={contato.container}>
+                <Text style={contato.text}>Contato</Text>
 
+                <Text style={contato.titles}>94875-6799</Text>
+                <Image style={contato.logos}
+                    source={{ uri: 'https://github.com/yurialvesyamada/infotec-react2.0/blob/redux/src/components/logos/whatsapp.png?raw=true' }}></Image>
+                <Text >
+
+                </Text>
+
+                <Text style={contato.titles}>@Infotec</Text>
+                <Image style={contato.logos}
+                    source={{ uri: 'https://github.com/yurialvesyamada/infotec-react2.0/blob/redux/src/components/logos/instagram.jpg?raw=true' }}></Image>
+                <Text >
+
+                </Text>
+
+                <Text style={contato.titles}>Infotec</Text>
+                <Image style={contato.logos}
+                    source={{ uri: 'https://github.com/yurialvesyamada/infotec-react2.0/blob/redux/src/components/logos/facebook.png?raw=true' }}></Image>
+                <Text >
+
+                </Text>
+
+                <Text style={contato.titles}>infotec.org@hotmail.com</Text>
+                <Image style={contato.logos}
+                    source={{ uri: 'https://github.com/yurialvesyamada/infotec-react2.0/blob/redux/src/components/logos/email.png?raw=true' }}></Image>
+                <Text >
+
+                </Text>
+            </View>
         </ScrollView>
     );
 };
 
 
-const product = StyleSheet.create({
+const contato = StyleSheet.create({
     container: {
         textAlign: 'center',
         alignItems: "center",
-        flex: 1,
-        justifyContent: 'center'
     },
     text: {
         fontSize: 35,
-        padding: 10,
+        margin: 30,
         color: '#17a2b8'
+    }, cards: {
+        alignItems: 'center',
+        padding: 5,
+    },
+    logos: {
+        width: 120,
+        height: 120,
+        margin: 20
+    },
+    titles: {
+        fontSize: 20,
+        color: 'black',
+        textAlign: 'center',
+        alignItems: "center",
     }
-
-
 });
 
 export default Contact;
